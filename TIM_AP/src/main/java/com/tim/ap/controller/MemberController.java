5dd687cc50c806e740ff0ffad527043593a69d5a
ByeongJunHwang <qudwnsghkd10@gmail.com>
2017-09-07 13:11:58 +0900

    멤버 엑셀 일괄등록(미완성) DRM문제

--- TIM_AP/src/main/java/com/tim/ap/controller/MemberController.java
+++ TIM_AP/src/main/java/com/tim/ap/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.tim.ap.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
@@ -14,6 +16,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -138,6 +143,39 @@ public class MemberController {
 		result.setViewName("/member/main");
 		return result;
 	}
+	
+	@RequestMapping("/excel")
+	public ModelAndView excelForm() {
+		
+		ModelAndView result = new ModelAndView();
+		
+		result.setViewName("/member/excel");
+		return result;
+	}
+
+	@ResponseBody
+    @RequestMapping(value = "/excelUploadAjax", method = RequestMethod.POST)
+    public ModelAndView excelUploadAjax(MultipartHttpServletRequest request)  throws Exception{
+        MultipartFile excelFile =request.getFile("excelFile");
+        System.out.println("엑셀 파일 업로드 컨트롤러");
+        if(excelFile==null || excelFile.isEmpty()){
+            throw new RuntimeException("엑셀파일을 선택 해 주세요.");
+        }
+        
+        File destFile = new File("D:\\"+excelFile.getOriginalFilename());
+        try{
+            excelFile.transferTo(destFile);
+        }catch(IllegalStateException | IOException e){
+            throw new RuntimeException(e.getMessage(),e);
+        }
+        
+        memberService.excelUpload(destFile);
+        
+        
+        ModelAndView view = new ModelAndView();
+        view.setViewName("/member/excel");
+        return view;
+    }
 	
 	
 }
--- TIM_AP/src/main/java/com/tim/ap/mapper/MemberMapper.java
+++ TIM_AP/src/main/java/com/tim/ap/mapper/MemberMapper.java
@@ -1,6 +1,7 @@
 package com.tim.ap.mapper;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.tim.ap.entity.MemberEntity;
 
@@ -8,6 +9,7 @@ public interface MemberMapper {
 	    ArrayList<MemberEntity> getMemberList();
 	    MemberEntity getMember(int id);
 	    void insertMember(MemberEntity memberEntity);
+	    void excelUpload(List<MemberEntity> memberList);
 	    //void updateMember(String id);
 	    //void deleteMember(String id);
 }
