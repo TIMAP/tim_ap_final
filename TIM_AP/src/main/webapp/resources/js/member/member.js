8db15671c7b1ed6bf254e8932e7759308754c58a
ByeongJunHwang <qudwnsghkd10@gmail.com>
2017-09-13 14:03:19 +0900

    다시했어요 다시받아요 이걸로

---
+++ TIM_AP/src/main/webapp/resources/js/member/member.js
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+
+function check() {
+    var file = $("#excelFile").val();
+    if (file == "" || file == null) {
+        alert("파일을 선택해주세요.");
+        return false;
+    } 
+
+    if (confirm("업로드 하시겠습니까?")) {
+        var options = {
+            success : function(data) {
+                alert("모든 데이터가 업로드 되었습니다.");
+            },
+            type : "POST",
+            error:function(){
+            	alert("이미 등록된 사용자가 존재합니다");
+            }
+        };
+        $("#excelUploadForm").ajaxSubmit(options);
+
+    }
+    
+    
+}
+
+function checkCsv() {
+    var file = $("#csvFile").val();
+    if (file == "" || file == null) {
+        alert("파일을 선택해주세요.");
+        return false;
+    } 
+
+    if (confirm("업로드 하시겠습니까?")) {
+        var options = {
+            success : function(data) {
+                alert("모든 데이터가 업로드 되었습니다.");
+
+            },
+            type : "POST",
+            error:function(){
+            	alert("이미 등록된 사용자가 존재합니다");
+            }
+        };
+        $("#csvInsert").ajaxSubmit(options);
+
+    }
+}
